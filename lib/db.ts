a598d610777eadc1e452ffde90d02efeae4ccc07
trals17 <slartler17@gmail.com>
2025-05-21 21:04:26 +0900

    16.1 Deployment

--- lib/db.ts
+++ lib/db.ts
@@ -1,18 +1,13 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
+// PrismaClient 인스턴스 생성
 const db = new PrismaClient();
 
-async function test() {
-  const token = await db.sMSToken.findUnique({
-    where: {
-      id: 2,
-    },
-    include: {
-      user: true,
-    },
-  });
-  console.log(token);
-}
-test();
+// 실제 코드에서는 DB 접근 테스트용 코드를 실행하지 않습니다.
+// if (process.env.NODE_ENV === 'development') {
+//   db.sMSToken.findUnique({ where: { id: 2 }, include: { user: true } })
+//     .then(token => console.log(token))
+//     .catch(err => console.error(err));
+// }
 
 export default db;
