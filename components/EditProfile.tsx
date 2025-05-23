97170a1e92d9075f9e753e7592a4ec1d319fea0b
trals17 <slartler17@gmail.com>
2025-05-23 15:09:21 +0900

    WIP

--- components/EditProfile.tsx
+++ components/EditProfile.tsx
@@ -5,7 +5,9 @@ import { getUploadUrl } from "@/app/products/add/actions";
 import Image from "next/image";
 import { useState } from "react";
 import { useFormState } from "react-dom";
+import { XMarkIcon } from "@heroicons/react/24/solid";
 import Input from "./input";
+import Link from "next/link";
 import BeforePage from "./BeforePage";
 
 interface EditProfileProps {
@@ -17,10 +19,7 @@ interface EditProfileProps {
 }
 
 export default function EditProfile({ user }: EditProfileProps) {
-  // 1) 기본 아바타
-  const defaultAvatar = user.avatar || "/images/default-avatar.png";
-  // 2) preview 초기값을 기본 아바타로
-  const [preview, setPreview] = useState<string>(defaultAvatar);
+  const [preview, setPreview] = useState<string | null>(null);
   const [uploadUrl, setUploadUrl] = useState("");
 
   const onImageChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -32,11 +31,9 @@ export default function EditProfile({ user }: EditProfileProps) {
     if (file.size > 2 * 1024 * 1024) {
       return alert("2MB를 초과하는 이미지는 업로드 할 수 없습니다.");
     }
-
     // 미리보기
     const url = URL.createObjectURL(file);
     setPreview(url);
-
     // 업로드 URL 받아오기
     const { result, success } = await getUploadUrl();
     if (success) {
@@ -47,7 +44,7 @@ export default function EditProfile({ user }: EditProfileProps) {
   const interceptAction = async (_prevState: any, formData: FormData) => {
     const file = formData.get("avatar");
     if (file instanceof File) {
-      // Cloudflare에 파일 POST
+      // 1) Cloudflare에 파일 POST
       const cfForm = new FormData();
       cfForm.append("file", file);
       const res = await fetch(uploadUrl, {
@@ -60,14 +57,15 @@ export default function EditProfile({ user }: EditProfileProps) {
       const publicUrl =
         data.result?.variants?.[0] ||
         `https://imagedelivery.net/${process.env.NEXT_PUBLIC_CLOUDFLARE_ACCOUNT_HASH}/${imageId}/public`;
+      // formData 갱신
       formData.set("avatar", publicUrl);
       setPreview(publicUrl);
     } else {
-      // 이미지 변경 없으면 기존 URL
+      // 변경된 이미지 없으면 기존 URL 그대로
       formData.set("avatar", user.avatar || "");
     }
 
-    // 서버 액션에는 FormData만 넘기기
+    // 서버 액션 호출 (FormData만 넘깁니다)
     return EditProfileAction(formData);
   };
 
@@ -76,7 +74,7 @@ export default function EditProfile({ user }: EditProfileProps) {
   return (
     <div className="p-5 flex flex-col gap-5">
       <form action={action} method="post" encType="multipart/form-data">
-        {/* 상단 바 */}
+        {/* 헤더 */}
         <div className="flex justify-between items-center">
           <BeforePage />
           <h3 className="text-2xl font-semibold">프로필 수정</h3>
@@ -88,24 +86,28 @@ export default function EditProfile({ user }: EditProfileProps) {
           </button>
         </div>
 
-        {/* 아바타 미리보기 & 입력 */}
+        {/* 아바타 업로드 & 미리보기 */}
         <div className="flex justify-center items-center mt-10">
           <label className="cursor-pointer" htmlFor="avatar-input">
-            <div
-              className="rounded-full overflow-hidden"
-              style={{
-                width: 80,
-                height: 80,
-                backgroundColor: "#eee",
-              }}
-            >
+            {preview ? (
+              <div
+                className="rounded-full"
+                style={{
+                  backgroundImage: `url(${preview})`,
+                  backgroundSize: "cover",
+                  width: "80px",
+                  height: "80px",
+                }}
+              />
+            ) : (
               <Image
-                src={preview}
-                alt="avatar preview"
+                src={user.avatar!}
+                alt={user.username}
                 width={80}
                 height={80}
+                className="rounded-full"
               />
-            </div>
+            )}
           </label>
           <input
             onChange={onImageChange}
@@ -117,11 +119,16 @@ export default function EditProfile({ user }: EditProfileProps) {
           />
         </div>
 
+        {/* 숨겨진 ID 필드 */}
+        <input type="hidden" name="id" value={user.id} />
+
         {/* 닉네임 입력 */}
         <div className="flex flex-col gap-3 mt-6">
-          <input type="hidden" name="id" value={user.id} />
-          <h4 className="font-medium">닉네임</h4>
+          <label htmlFor="username" className="font-medium">
+            닉네임
+          </label>
           <Input
+            id="username"
             name="username"
             required
             placeholder="username"
