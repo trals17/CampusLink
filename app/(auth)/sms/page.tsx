274c0bcbfd4644ab9d08199fefc9567533f5e998
trals17 <slartler17@gmail.com>
2025-06-03 18:38:05 +0900

    WIP

--- app/(auth)/sms/page.tsx
+++ app/(auth)/sms/page.tsx
@@ -14,10 +14,7 @@ export default function SMSLogin() {
   const [state, dispatch] = useFormState(smsLogIn, initialState);
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
-      <div className="flex flex-col gap-2 *:font-medium">
-        <h1 className="text-2xl">SMS Log in</h1>
-        <h2 className="text-xl">Verify your phone number.</h2>
-      </div>
+      {/* ... */}
       <form action={dispatch} className="flex flex-col gap-3">
         {state.token ? (
           <Input
@@ -38,7 +35,10 @@ export default function SMSLogin() {
             errors={state.error?.formErrors}
           />
         )}
-        <Button text={state.token ? "Verify Token" : "Send Verification SMS"} />
+        <Button
+          type="submit"
+          text={state.token ? "Verify Token" : "Send Verification SMS"}
+        />
       </form>
     </div>
   );
